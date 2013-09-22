9f043e0eefdbcf86239767ab99362d267e4a96bc
Chacha Sikes <chachasikes@gmail.com>
2013-09-21 17:46:45 -0700

    switched slider and date picker to jquery ui

--- public/js/custom/index.js
+++ public/js/custom/index.js
@@ -1,16 +1,53 @@
-$('.datepicker').datepicker({
-    format: 'mm-dd-yyyy'
-});
+$( "#datepicker-start" ).datepicker();
+$( "#datepicker-end" ).datepicker();
+$( "#slider" ).slider({
+      value:100,
+      min: 0,
+      max: 500,
+      step: 50,
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.value );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider" ).slider( "value" ) );
+    
 
 
-
+// Load map
 var map = L.mapbox.map('map', 'examples.map-vyofok3q').setView([37.5, -118], 6);
 
+// Make aquifer layer.
 var layer = L.geoJson(null, { style: { color: '#333', weight: 1 }});
-
 map.addLayer(layer);
 
+// Load aquifers (topoJSON)
 d3.json('../../data/topojson/dwr_basin_boundaries.json', function(error, data) {
   var basins = topojson.feature(data, data.objects.database);          
   layer.addData(basins);
 })
+
+
+// load all wells by default -- default date range
+
+
+// Geocode address.
+// http://www.gisgraphy.com/documentation/user-guide.htm#geocodingwebservice
+
+// on submit
+// get address
+// geocode it
+// get lat lon
+// get current date range
+// default interval - 365
+// construct search query
+// get back results
+// start map at the beginning
+
+// plot wells
+// add numbers to wells
+// by subbasin, construct average number
+// plot color
+// set color of aquifer
+
+// on change time slider, load selected increment
+// update map
