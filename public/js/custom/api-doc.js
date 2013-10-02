b5bd05ad97313319d1e05b5e0469eb2517155a1d
videmsky <stuffaccounts@gmail.com>
2013-10-02 11:48:59 -0700

    added js for read more

---
+++ public/js/custom/api-doc.js
@@ -0,0 +1,43 @@
+!function ($) {
+  
+  $(function(){
+
+		function load_ui (){
+			var el, ps, up, totalHeight;
+
+			$(".teaser-text-fade a").click(function() {
+						
+				totalHeight = 0
+
+				el = $(this);
+				p  = el.parent();
+				up = p.parent();
+				ps = up.find(".test");
+				
+				// measure how tall inside should be by adding together heights of all inside paragraphs (except read-more paragraph)
+				ps.each(function() {
+					totalHeight += $(this).outerHeight();
+				});
+							
+				up
+					.css({
+						// Set height to prevent instant jumpdown when max height is removed
+						"height": up.height(),
+						"max-height": 9999
+					})
+					.animate({
+						"height": totalHeight
+					});
+				
+				// fade out read-more
+				p.fadeOut();
+				
+				// prevent jump-down
+				return false;
+			});
+		}
+		$(document).ready(load_ui);
+
+  })
+
+}(window.jQuery)
