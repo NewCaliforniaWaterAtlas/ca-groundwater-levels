4dc7e590f83dbf9ee233a1c72f2ca6b718217b08
videmsky <stuffaccounts@gmail.com>
2013-10-06 20:01:47 -0700

    replaced .test class

--- public/js/custom/api-doc.js
+++ public/js/custom/api-doc.js
@@ -12,7 +12,7 @@
 				el = $(this);
 				p  = el.parent();
 				up = p.parent();
-				ps = up.find(".test");
+				ps = up.find(".teaser-text");
 				
 				// measure how tall inside should be by adding together heights of all inside paragraphs (except read-more paragraph)
 				ps.each(function() {
